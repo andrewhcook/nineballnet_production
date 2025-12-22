c9a819003eca4152e798cec42b81fd7747614735
unknown <andrewhcook1@gmail.com>
2025-12-22 17:45:20 -0600

    uploading new client to nineballnet

--- nineballnet/assets/static/pkg/nineball_wasm.d.ts
+++ nineballnet/assets/static/pkg/nineball_wasm.d.ts
@@ -26,9 +26,9 @@ export interface InitOutput {
   readonly wasm_bindgen__closure__destroy__h19702a69851894c4: (a: number, b: number) => void;
   readonly wasm_bindgen__convert__closures_____invoke__hbac0acc1adf59861: (a: number, b: number, c: any) => void;
   readonly wasm_bindgen__closure__destroy__h5f8cf5c1ce1eb0db: (a: number, b: number) => void;
-  readonly wasm_bindgen__convert__closures_____invoke__he33ab63df9443194: (a: number, b: number, c: any, d: any) => void;
   readonly wasm_bindgen__convert__closures_____invoke__h95336bcd11dd760f: (a: number, b: number) => void;
   readonly wasm_bindgen__closure__destroy__h258e951ddd50f47c: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures_____invoke__he33ab63df9443194: (a: number, b: number, c: any, d: any) => void;
   readonly wasm_bindgen__convert__closures_____invoke__he0d172362c95ddc2: (a: number, b: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
